94ec5a7ced9fc66245081417de2ad252b5863ce8
Crislzno <lozanobautistacristina@gmail.com>
2023-03-23 11:00:04 +0100

    Update Health.cs

--- Assets/Scripts/Jugador/Health.cs
+++ Assets/Scripts/Jugador/Health.cs
@@ -20,7 +20,7 @@ public class Lives : MonoBehaviour
 
             if (health <= 0f)
             {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex );
             }
         }
 
