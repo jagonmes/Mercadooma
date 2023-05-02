5153bac86d17591716af298152295e0ccb82f24b
Crislzno <lozanobautistacristina@gmail.com>
2023-05-02 18:41:42 +0200

    Comportamiento 2

---
+++ Assets/Scripts/Enemy/EnemyShoot.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour
+{
+    public GameObject enemyBullet; //prefab bala
+
+    public Transform spawnBulletPoint; //posición desde la que sale la bala
+
+    private Transform playerPosition;
+
+    public float bulletvelocity; 
+    void Start()
+    {
+        playerPosition = GameObject.Find("Jugador").transform;
+
+        Invoke("ShootPlayer", 3);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void ShootPlayer()
+    {
+        Vector3 playerDirection = playerPosition.position - transform.position;
+        GameObject newBullet;
+
+        newBullet = Instantiate(enemyBullet, spawnBulletPoint.position, spawnBulletPoint.rotation);
+
+        newBullet.GetComponent<Rigidbody>().AddForce(playerDirection*bulletvelocity,ForceMode.Force);
+        Invoke("ShootPlayer", 3);
+    }
+}
