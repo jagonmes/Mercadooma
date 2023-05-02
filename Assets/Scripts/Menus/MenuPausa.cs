4461dba878d32570d207b6489830eab09fa13fa6
Mdoc14 <marcos.deozaeta@gmail.com>
2023-05-03 01:28:11 +0200

    Añadido menú ajustes

--- Assets/Scripts/Menus/MenuPausa.cs
+++ Assets/Scripts/Menus/MenuPausa.cs
@@ -2,19 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class MenuPausa : MonoBehaviour
 {
-    [SerializeField] private GameObject botonPausa;
 
     [SerializeField] private GameObject menuPausa;
+
+    [SerializeField] private GameObject menuAjustes;
+
+    public Image bSonido;
+    public Image bMusica;
+
+    public Sprite soundOn;
+    public Sprite soundOff;
 
     public static bool juegoPausado = false;
     
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha0))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             if(juegoPausado)
             {
@@ -35,24 +43,61 @@ public class MenuPausa : MonoBehaviour
         Cursor.visible = true;
         juegoPausado = true;
         Time.timeScale = 0f;
-        botonPausa.SetActive(true);
         menuPausa.SetActive(true);
     }
    
     public void Reanudar()
-     {
+    {
         Cursor.visible = false;
         juegoPausado = false;
         Time.timeScale = 1f;
-        botonPausa.SetActive(false);
         menuPausa.SetActive(false);
-     }
+        menuAjustes.SetActive(false);
+    }
+
     public void Reiniciar()
-     {
+    {
         juegoPausado = false;
         Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-     }
+    }
+
+    public void AbrirAjustes()
+    {
+        menuPausa.SetActive(false);
+        menuAjustes.SetActive(true);
+    }
+
+    public void CerrarAjustes()
+    {
+        menuPausa.SetActive(true);
+        menuAjustes.SetActive(false);
+    }
+
+    public void botonSonido() 
+    {
+        if (bSonido.sprite == soundOn)
+        {
+            bSonido.sprite = soundOff;
+        }
+        else 
+        {
+            bSonido.sprite = soundOn;
+        }
+    }
+
+    public void botonMusica() 
+    {
+        if (bMusica.sprite == soundOn)
+        {
+            bMusica.sprite = soundOff;
+        }
+        else
+        {
+            bMusica.sprite = soundOn;
+        }
+    }
+
     public void Cerrar()
     {
         Debug.Log("Cerrando Juego...");
